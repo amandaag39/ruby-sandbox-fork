44629ca251aeb08f5eff334f77e1510bced85bf7
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2023-11-13 18:40:33 +0000

    thinkfast exercise start commit

---
+++ thinkfast.rb
@@ -0,0 +1,31 @@
+unpredictable_inputs = [
+  "Hello!",
+  Time.now,
+  rand(100),
+  :GOODBYE,
+  nil,
+  true,
+  false,
+  { :city => "Chicago", :state => "IL", :zip => 60654 }
+]
+some_random_input = unpredictable_inputs.sample
+
+pp some_random_input.class
+
+if some_random_input.class == String
+  pp some_random_input.downcase
+elsif some_random_input.class == Time
+  days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  now = Time.now
+  day_of_week = now.wday
+  pp days_of_week[day_of_week]
+elsif some_random_input.class == Integer
+  if some_random_input % 2 == 0
+    pp "#{some_random_input} is even"
+  else
+    pp "#{some_random_input} is odd"
+  end
+elsif
+else
+  pp "The input wasn't a string"
+end
